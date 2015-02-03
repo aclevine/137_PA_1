a56868efc0333a01fd6b66404ad3fa6f49455ebf
Zachary Yocum <zachary.yocum@me.com>
2015-02-02 20:47:40 -0500

    Added word2vec cluster resource loader and feature extractor

--- dev/prepdata.py
+++ dev/prepdata.py
@@ -2,7 +2,10 @@
 import os
 from csv import *
 from string import *
-from operator import itemgetter, attrgetter, methodcaller
+
+# Some special unicode characters
+currencies = u'$¢£¤¥ÐŁƒɃ৳฿ლ₡₤₥₦₨₩₪₫€₭₮₱₲₴₽元円'
+punctuation += u'‐-‑⁃֊᠆‧·¡!¿?⸘‽“”‘’‛‟.,‚„′″´˝^°¸˛¨`˙˚ªº…&_¯­–‑—§⁊¶†‡@‰‱¦ˉˆ˘ˇ‼︎⁇⁈⁉︎❛❜❝❞❢❣❡'
 
 # LOAD RESOURCES
 def load_brown_clusters(brown_input_path):
@@ -25,6 +28,15 @@ def load_gazetteer(gaze_input_path):
                 gazetteer[word] = gazetteer.get(word, []) + [entry]
     return gazetteer
 
+def load_w2v_clusters(w2v_input_path):
+    with open(w2v_input_path, 'rb') as file:
+        lines = filter(None, file.read().split('\n'))
+    return dict(line.split() for line in lines)
+
+brown_dict = load_brown_clusters('./resources/brown_clusters.txt')
+gazetteer = load_gazetteer('./resources/named_entity_lists/eng.list') # named entity dict
+w2v_dict = load_w2v_clusters('./resources/words2vec-clusters.txt')
+
 class ACEDialect(Dialect):
     """A CSV dialect for reading ACE BIO/POS data."""
     delimiter = '\t'
@@ -33,15 +45,16 @@ class ACEDialect(Dialect):
     quoting = QUOTE_NONE
     skipinitialspace = True
 
-currencies = u'$Â¢Â£Â¤Â¥ÃÅÆ’Éƒà§³à¸¿áƒšâ‚¡â‚¤â‚¥â‚¦â‚¨â‚©â‚ªâ‚«â‚¬â‚­â‚®â‚±â‚²â‚´â‚½å…ƒå††'
-punctuation += u'â€-â€‘âƒÖŠá †â€§Â·Â¡!Â¿?â¸˜â€½â€œâ€â€˜â€™â€›â€Ÿ.,â€šâ€žâ€²â€³Â´Ë^Â°Â¸Ë›Â¨`Ë™ËšÂªÂºâ€¦&_Â¯Â­â€“â€‘â€”Â§âŠÂ¶â€ â€¡@â€°â€±Â¦Ë‰Ë†Ë˜Ë‡â€¼ï¸Žâ‡âˆâ‰ï¸Žâ›âœââžâ¢â£â¡'
-brown_dict = load_brown_clusters('./resources/brown_clusters.txt')
-gazetteer = load_gazetteer('./resources/named_entity_lists/eng.list') # named entity dict
 register_dialect('ace', ACEDialect)
+
 #===============================================================================
 
 class Token(object):
-    """docstring for Token"""
+    """A token is a tuple-like object with four attributes:
+        index : an integer representing the index of the token in the sentence
+        text  : a string representation of the token
+        pos   : a string representing the POS tag of the token
+        bio   : a string representing the BIO tag"""
     def __init__(self, index, text, pos, bio):
         self.index = index
         self.text = text
@@ -52,12 +65,12 @@ class Token(object):
         return '{}({})'.format(self.__class__.__name__, self._args())
     
     def _args(self):
-        """Returns an ordered list of t for string representation."""
+        """Returns an ordered list of arguments for string representation."""
         args = self.index, self.text, self.pos, self.bio
         return ', '.join(map(repr, args))
 
 class Sentence(tuple):
-    """docstring for Sentence"""
+    """A tuple wrapper class for working with sequences of Tokens."""
     def __init__(self, tokens=tuple()):
         if not all(isinstance(item, Token) for item in tokens):
             raise TypeError, 'expected a sequence of Tokens'
@@ -79,6 +92,9 @@ class Sentence(tuple):
     def __repr__(self):
         return '<{} with {} tokens>'.format(self.__class__.__name__, len(self))
     
+    def __str__(self):
+        return ' '.join(t.text.encode('utf-8') for t in self)
+    
     def featurize(self, features, window=range(1)):
         #Preciously: def featurize(self, features, window=range(1)):
         #Chen: I added NEDict as a parameter for the method resourceConfirm();
@@ -86,20 +102,21 @@ class Sentence(tuple):
             raise ValueError, 'window must be a list of integers'
         if not all(isinstance(item, int) for item in window):
             raise ValueError, 'window must be a list of integers'
-        bounds = range(len(self))
-        for index, item in enumerate(self):
+        bounds = set(range(len(self)))
+        for index, item in enumerate(self): # sequence of Token objs
             values = [item.bio]
             context = [index + offset for offset in window]
-            f_dict = dict((f.__name__, f) for f in features)
-            for feature, function in f_dict.iteritems():
+            for f in features:
                 for i in context:
                     if i in bounds:
-                        value = function(self, i)
+                        value = f(self, i)
                         if isinstance(value, basestring):
-                            for char in '=:':
+                            # ':' and '=' are special delimiters
+                            for char in set('=:'):
                                 value.replace(char, '.')
-                        label = u'{}[{}]={}'.format(feature, i-index, u'{}')
-                        # ':' and '=' are special delimiters
+                            if not value:
+                                continue
+                        label = u'{}[{}]={}'.format(f.__name__, i-index, '{}')
                         values.append(label.format(value).replace(':', '.'))
             yield values
 
@@ -141,7 +158,13 @@ def trigram(sent, i):
     start = max(0, i-1)
     end = min(i+2, len(sent))
     return '_'.join([t.text for t in sent[start:end]])
-    
+
+def trigram_pos(sent, i):
+    """POS tags for previous, target, and next token"""
+    start = max(0, i-1)
+    end = min(i+2, len(sent))
+    return '_'.join([t.pos for t in sent[start:end]])
+
 def prev_trigram(sent, i):
     """target and 2 preceding tokens in sentence"""
     return trigram(sent, i-1)
@@ -149,6 +172,18 @@ def prev_trigram(sent, i):
 def next_trigram(sent, i):
     """target and 2 following tokens in sentence"""
     return trigram(sent, i+1)
+
+def prev_bigram_pos(sent, i):
+    """previous pos and target pos"""
+    start = max(0, i-1)
+    end = min(i+1, len(sent))
+    return '_'.join([t.pos for t in sent[start:end]])
+
+def next_bigram_pos(sent, i):
+    """previous pos and target pos"""
+    start = max(0, i+1)
+    end = min(i+1, len(sent))
+    return '_'.join([t.pos for t in sent[start:end]])
 
 def entity_type(sent, i):
     """check token against gazetteer"""
@@ -166,14 +201,19 @@ def entity_type(sent, i):
 def brown_cluster_id(sent, i):
     token = sent[i]
     return brown_dict.get(token.text, -1)
-    
+
+def w2vcluster(sent, i):
+    """Returns the word2vec cluster ID of the token."""
+    token = text(sent[i])
+    return w2v_dict.get(text(token).lower(), -1)
+
 def unigram(sent, i):
     return text(sent[i])
 
 def nopunct(sent, i):
     """The token itself stripped of leading/trailing punctuation."""
     token = sent[i]
-    return text(token).strip(punctuation)
+    return text(token).strip((punctuation))
 
 def pos(sent, i):
     """The part-of-speech tag."""
@@ -181,21 +221,13 @@ def pos(sent, i):
     if isinstance(token, Token):
         return token.pos
 
-def orth(sent, i):
-    """A binary representation of the unicode.
-    E.g.:
-        u'...' -> '000'
-        u'abc' -> '000'
-        u'abC' -> '001'
-        u'AbÃ§' -> '100'
-        u'AbC' -> '101'
-        u'Ã€BC' -> '111'
-    """
+def length(sent, i):
+    """The number of characters in the token."""
     token = sent[i]
-    return ''.join(map(str,map(int,(map(unicode.isupper, text(token))))))
+    return len(text(token))
 
 def cap(sent, i):
-    """True if every character in the token is capitalized, otherwise False."""
+    """True if every character in the token is capitalized, False otherwise."""
     token = sent[i]
     return unicode.isupper(text(token))
 
@@ -236,7 +268,17 @@ def tail(sent, i):
 def shape(sent, i):
     """Return a general shape of the token.
     
-    E.g., 'Abc' -> '
+    E.g.:
+        'Abc'  -> 'Xxx'
+        'ABC'  -> 'XXX'
+        'Àbc'  -> 'Uxx'
+        '2015' -> 'dddd'
+        'ab12' -> 'xxdd'
+        'àçè'  -> 'uuu'
+        'ÀÇÈ'  -> 'UUU'
+        '€10'  -> '$dd'
+        ' a '  -> 'x'
+        '-a-'  -> '.x.'
     """
     token = sent[i]
     chars = []
@@ -259,6 +301,9 @@ def shape(sent, i):
             chars += 'u'
     return u''.join(chars)
 
+def simple_shape(sent, i):
+    token = sent[i]
+    return ''.join(('X' if char.isupper() else 'x') for char in text(token))
 
 #===============================================================================
 def write_crf_data(in_path, out_path, features):
@@ -269,7 +314,7 @@ def write_crf_data(in_path, out_path, features):
             sentence = Sentence()
             for row in csv_reader:
                 if not row:
-                    for line in sentence.featurize(features, range(-2,3)):
+                    for line in sentence.featurize(features, range(-1,2)):
                     #Previous: for line in sentence.featurize(features, range(-2,3)):
                     #Chen: I added a NEDict for the featurize function I changed.
                         csv_writer.writerow(line)
@@ -285,22 +330,25 @@ def write_sent_data(in_path, out_path):
             sentence = Sentence()
             for row in csv_reader:
                 if not row:
-                    csv_writer.writerow([' '.join([token.text for token in sentence])])
+                    csv_writer.writerow(
+                        [' '.join(token.text for token in sentence)]
+                    )
                     sentence = Sentence()
                 else:
                     sentence += Token(*row)
 
 if __name__ == '__main__':
-
     train_path = os.path.join('resources', 'project1-train-dev', 'train.gold')
     dev_path = os.path.join('resources', 'project1-train-dev', 'dev.gold')
     crf_train = 'train.crfsuite.txt'
     crf_test = 'dev.crfsuite.txt'
-    features = unigram, nopunct, pos, cap, title, alnum, num, first, tail, shape, trigram,\
-                entity_type
+    features = pos, cap, title, alnum, num, alpha, nopunct, first, length, \
+               shape, simple_shape, \
+               entity_type, brown_cluster_id, w2vcluster, \
+               trigram_pos, trigram, \
+               next_bigram_pos, next_bigram, \
+               prev_bigram_pos, prev_bigram
     write_crf_data(train_path, crf_train, features)
     write_crf_data(dev_path, crf_test, features)
     
     #write_sent_data('./resources/project1-train-dev/dev.gold, 'sent.txt')
-    
-    print gazetteer['kurt']
